acd53cd85de37e19f5cff06247aa3508ad35cf2b
wertas441 <wertas.borisov@gmail.com>
2025-12-08 09:59:09 +0300

    develop: add new exercises and some UX fixes

--- frontend/src/components/UI/sidebars/MainSideBar.tsx
+++ frontend/src/components/UI/sidebars/MainSideBar.tsx
@@ -9,6 +9,7 @@ import {
     XMarkIcon
 } from "@heroicons/react/24/outline";
 import {useMemo} from "react";
+import AnyStylesButton from "@/components/buttons/other/AnyStylesButton";
 
 interface SideBarProps {
 	activePage: string;
@@ -81,24 +82,26 @@ export default function MainSideBar({ activePage, isOpen = false, onClose }: Sid
 				<div
 					className="fixed inset-0 bg-black/30 backdrop-blur-[1px] lg:hidden z-40"
 					aria-hidden="true"
+					onClick={onClose}
 				/>
 			)}
 
 			<aside
 				className={`
 				fixed lg:static top-0 left-0 h-full lg:h-auto
-				w-72 border-r border-emerald-100 p-4 bg-white
+				w-72 border-r border-emerald-100 px-4 pt-2 bg-white
 				transform transition-transform duration-300 ease-in-out z-50
 				${isOpen ? 'translate-x-0' : '-translate-x-full'} lg:translate-x-0
 			`}
 				aria-label="Боковая навигация"
 			>
 
-                <div className="lg:hidden flex justify-between items-center pb-3 ">
-                    <h1 className={`lg:text-2xl md:text-2xl text-xl font-semibold text-emerald-800`}>TrainingSpace</h1>
-                    <button onClick={onClose} className={``}>
-                        <XMarkIcon className={`h-7 w-7 mr-2 `} />
-                    </button>
+                <div className="lg:hidden flex justify-between items-center pb-5 ">
+                    <h1 className={`text-2xl pt-1 font-semibold text-emerald-800`}>TrainingSpace</h1>
+                    <AnyStylesButton
+                        onClick={onClose}
+                        IconComponent={XMarkIcon}
+                    />
                 </div>
 
 				<nav className="mt-6 list-none space-y-5 ">
